13e8bdefcd3f2a87e0711cf40ca2d50406e091ca
Stephen Ruan <Stephen@Stephens-MacBook-Pro.local>
2018-06-11 13:08:44 -0700

    iphone

--- src/pages/Location/Location.js
+++ src/pages/Location/Location.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 
 class Location extends Component {
+
+   mapsSelector = (e) => {
+   	e.preventDefault();
+      if /* if we're on iOS, open in Apple Maps */
+    ((navigator.platform.indexOf("iPhone") != -1) || 
+     (navigator.platform.indexOf("iPad") != -1) || 
+     (navigator.platform.indexOf("iPod") != -1))
+    window.open("maps://maps.google.com/maps?daddr=Burger Shack, 1911, 2237 Powell St, San Francisco, CA 94133");
+		else /* else use Google */
+    window.open("https://www.google.com/maps/place/Burger+Shack/@37.806552,-122.4118016,18z/data=!4m5!3m4!1s0x808580fb28ee016d:0x4bc1151d3b734095!8m2!3d37.8055009!4d-122.4120484");
+	}
 	render() {
 		return (
 			<div className='location'>
 			<div className='row'>
 				<div className='col-sm-12 col-md-6'>
-					<a href='https://www.google.com/maps/place/Burger+Shack/data=!3m1!4b1!4m2!3m1!1s0x808580fb28ee016d:0x4bc1151d3b734095' target='_blank'><img src='https://maps.googleapis.com/maps/api/staticmap?scale=1&size=520x410&style=feature:poi.business|visibility:off&style=feature:water|visibility:simplified&style=feature:road|element:labels.icon|visiblity:off&style=feature:road.highway|element:labels|saturation:-90|lightness:25&format=jpg&language=en&region=US&markers=color:0xddaa44|37.8055009,-122.4120484&zoom=16&client=google-presto&signature=zx7YM_UM9u00e0PLorAmLJQjwEw'/></a>
+					<img src='https://maps.googleapis.com/maps/api/staticmap?scale=1&size=520x410&style=feature:poi.business|visibility:off&style=feature:water|visibility:simplified&style=feature:road|element:labels.icon|visiblity:off&style=feature:road.highway|element:labels|saturation:-90|lightness:25&format=jpg&language=en&region=US&markers=color:0xddaa44|37.8055009,-122.4120484&zoom=16&client=google-presto&signature=zx7YM_UM9u00e0PLorAmLJQjwEw'/>
 				</div>
 				<div className='col-sm-12 col-md-6'>
 					<h2 className='d-flex justify-content-center'>BURGER SHACK</h2>
@@ -21,7 +32,7 @@ class Location extends Component {
 									<br/>
 									CA, 94133
 									</p>
-								<button>Get Direction</button>
+								<button onClick={this.mapsSelector}><a href="#" target='_blank'>Get Direction</a></button>
 
 								<h3>Contact</h3>
 								<a href='tel:+1-415-523-6818'>(415)523-6818</a>
